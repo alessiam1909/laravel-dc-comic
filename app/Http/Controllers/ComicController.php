5864ffdd67fe117486a86499eb6f1206533d8bee
alessiam1909 <ale.menon.vr@gmail.com>
2023-02-24 10:03:14 +0100

    creazione del seeder e popolazione della tabella

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -7,12 +7,12 @@ use App\Models\Comic as Comic;
 
 class ComicController extends Controller
 {
-    public function getComic(){
-        $fumetti = Comic::all();
-        $nav_menu = config('db.nav');
-        $icons  =config('db.social_icons');
-        return view('home', compact('fumetti', 'nav_menu', 'icons'));
-    }
+    // public function getComic(){
+    //     $fumetti = Comic::all();
+    //     $nav_menu = config('db.nav');
+    //     $icons  =config('db.social_icons');
+    //     return view('home', compact('fumetti', 'nav_menu', 'icons'));
+    // }
 
     // public function getSingleComic($id){
     //     $fumetti = Comic::find($id);
